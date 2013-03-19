f76e6a4e42028fb3f0457bbca34fe83680ecce08
alan.silva <alan.silva@ebs.com.br>
2013-03-19 11:00:35 -0300

    Adicionando idade

--- Pessoa.cs
+++ Pessoa.cs
@@ -4,11 +4,13 @@ class Pessoa
 {
 	private string nome;
 	private string sobrenome;
+	private int idade;
 	
-	public Pessoa(string nome, string sobrenome)
+	public Pessoa(string nome, string sobrenome, int idade)
 	{
 		this.nome = nome;
 		this.sobrenome = sobrenome;
+		this.idade = idade;
 	}
 	
 	public void DigaOla()
